d01a478b1a9feaaa5601f91d9f8a405f625e43ac
Cole Herrmann <colewh2@illinois.edu>
2021-05-04 13:42:53 -0700

    Fixed file reading issues and each data element is added into the proper vector

--- graph.cpp
+++ graph.cpp
@@ -4,7 +4,7 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
-#include <graph.h>
+#include "graph.h"
 #include "edge.h"
 
 std::string graph::file_to_string(const std::string & filename) {
@@ -37,20 +37,28 @@ graph::graph(const std::string nodeInfo, const std::string edgeInfo) {
     for (unsigned i = 0; i < nodesI.size(); i++) {
         std::string row = nodesI[i];
         int count = 0;
-        istringstream iss(row);
+        std::istringstream iss(row);
+        std::string::size_type sz = 0;
+        std::string subs;
+
         do {
-            std::string subs;
             iss >> subs;
-            double num = double(subs);
+            std::cout << subs << std::endl;
             if (count % 3 == 0) {
+                double num = std::stod(&subs[0], &sz);
                 int n = int(num);
                 nodeID.push_back(n);
+                std::cout << n << std::endl;
             }
             if (count % 3 == 1) {
+                double num = std::stod(&subs[0], &sz);
                 xLocs.push_back(num);
+                std::cout << num << std::endl;
             }
             if (count % 3 == 2) {
+                double num = std::stod(&subs[0], &sz);
                 yLocs.push_back(num);
+                std::cout << num << std::endl;
             }
             count += 1;
         } while (iss);
@@ -59,30 +67,41 @@ graph::graph(const std::string nodeInfo, const std::string edgeInfo) {
     for (unsigned i = 0; i < edgeI.size(); i++) {
         std::string row = edgeI[i];
         int count = 0;
-        istringstream iss(row);
+        std::istringstream iss(row);
+        std::string::size_type sz = 0;
+        std::string subs;
+
         do {
-            std::string subs;
             iss >> subs;
-            double num = double(subs);
+            double num = std::stod(subs, &sz);
             int n = int(num);
             if (count % 4 == 0) {
                 //int n = int(num);
+                double n = std::stod(&subs[0], &sz);
                 edgeIDs.push_back(n);
+                std::cout << n << std::endl;
             }
             if (count % 4 == 1) {
-                startNode.push_back(n);
+                double n = std::stod(&subs[0], &sz);
+                edgeLength.push_back(n);
+                std::cout << n << std::endl;
             }
             if (count % 4 == 2) {
-                endNode.push_back(n);
+                double n = std::stod(&subs[0], &sz);
+                startNode.push_back(n);
+                std::cout << n << std::endl;
             }
             if (count % 4 == 3) {
-                edgeLength.push_back(num);
+                double n = std::stod(&subs[0], &sz);
+                endNode.push_back(n);
+                std::cout << n << std::endl;
             }
             count += 1;
         } while (iss);
     }
 
 }
+
 std::vector<int> graph::incidentEdges(int std::string) {
     std::vector<int> result;
     for (unsigned i = 0; i < edgeIDs.size(); i++) {
@@ -156,3 +175,4 @@ edge graph::getEdgeId(std::string start , std::string end) const
     Edge e = adjacency[source][destination].getId();
     return e;
 }
+
