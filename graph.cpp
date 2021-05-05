3cb73a1af6477ae47d890bda4a5da0892c35ca8d
Cole Herrmann <colewh2@illinois.edu>
2021-05-05 15:31:40 -0700

    File loading issues all resolved

--- graph.cpp
+++ graph.cpp
@@ -20,16 +20,12 @@ std::string graph::file_to_string(const std::string & filename) {
 std::vector<std::string> graph::file_to_vector(const std::string & filename) {
     std::ifstream text(filename);
 	std::vector<std::string> out;
+    std::string str;
 
-	if (text.is_open()) {
-		std::istream_iterator<std::string> iter(text);
-		while (!text.eof()) {
-			out.push_back(*iter);
-			++iter;
-		}
-	}
-
-	return out;
+    while (std::getline(text, str)){
+        if (str.size() > 0) out.push_back(str);
+    }
+    return out;
 }
 graph::graph(const std::string nodeInfo, const std::string edgeInfo) {
     std::vector<std::string> nodesI = file_to_vector(nodeInfo);
@@ -43,23 +39,24 @@ graph::graph(const std::string nodeInfo, const std::string edgeInfo) {
 
         do {
             iss >> subs;
-            std::cout << subs << std::endl;
+           // std::cout << subs << std::endl;
             if (count % 3 == 0) {
                 double num = std::stod(&subs[0], &sz);
                 int n = int(num);
                 nodeID.push_back(n);
-                std::cout << n << std::endl;
+                //std::cout << n << std::endl;
             }
             if (count % 3 == 1) {
                 double num = std::stod(&subs[0], &sz);
                 xLocs.push_back(num);
-                std::cout << num << std::endl;
-            }
-            if (count % 3 == 2) {
-                double num = std::stod(&subs[0], &sz);
+                //std::cout << num << std::endl;
+
+                iss >> subs;
+                num = std::stod(&subs[0], &sz);
                 yLocs.push_back(num);
-                std::cout << num << std::endl;
+               // std::cout << num << std::endl;
             }
+            
             count += 1;
         } while (iss);
 
@@ -79,29 +76,33 @@ graph::graph(const std::string nodeInfo, const std::string edgeInfo) {
                 //int n = int(num);
                 double n = std::stod(&subs[0], &sz);
                 edgeIDs.push_back(n);
-                std::cout << n << std::endl;
+                //std::cout << n << std::endl;
             }
             if (count % 4 == 1) {
                 double n = std::stod(&subs[0], &sz);
                 edgeLength.push_back(n);
-                std::cout << n << std::endl;
+               //std::cout << n << std::endl;
             }
             if (count % 4 == 2) {
                 double n = std::stod(&subs[0], &sz);
                 startNode.push_back(n);
-                std::cout << n << std::endl;
-            }
-            if (count % 4 == 3) {
-                double n = std::stod(&subs[0], &sz);
+                //std::cout << n << std::endl;
+
+                iss >> subs;
+                n = std::stod(&subs[0], &sz);
                 endNode.push_back(n);
-                std::cout << n << std::endl;
+               // std::cout << n << std::endl;
             }
+            
             count += 1;
         } while (iss);
     }
+    
+
+
 
 }
-
+/*
 std::vector<int> graph::incidentEdges(int std::string) {
     std::vector<int> result;
     for (unsigned i = 0; i < edgeIDs.size(); i++) {
@@ -175,4 +176,4 @@ edge graph::getEdgeId(std::string start , std::string end) const
     Edge e = adjacency[source][destination].getId();
     return e;
 }
-
+*/
